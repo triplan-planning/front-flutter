1d9638166a35a55143676874c3cebb207c65c636
ComteHerrapait <leondecheux@gmail.com>
2022-08-27 23:49:26 +0200

    get users from API

--- lib/src/models/user.dart
+++ lib/src/models/user.dart
@@ -1,6 +1,13 @@
 class User {
-  const User(this.id, this.name);
+  const User({required this.id, required this.name});
 
   final String id;
   final String name;
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+    );
+  }
 }
--- lib/src/pages/user_list_view.dart
+++ lib/src/pages/user_list_view.dart
@@ -1,19 +1,32 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:triplan/src/models/user.dart';
 import 'package:triplan/src/pages/user_detail_view.dart';
 
 import '../settings/settings_view.dart';
 
-/// Displays a list of SampleItems.
-class UserListView extends StatelessWidget {
+import 'package:http/http.dart' as http;
+
+class UserListView extends StatefulWidget {
   const UserListView({
     Key? key,
-    this.items = const [User("093484bnqerg", "Jean-Rémi Fourage")],
   }) : super(key: key);
 
   static const routeName = '/';
 
-  final List<User> items;
+  @override
+  State<UserListView> createState() => _UserListViewState();
+}
+
+class _UserListViewState extends State<UserListView> {
+  late Future<List<User>> futureUsers;
+
+  @override
+  void initState() {
+    super.initState();
+    futureUsers = fetchUsers();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,30 +52,56 @@ class UserListView extends StatelessWidget {
       // In contrast to the default ListView constructor, which requires
       // building all Widgets up front, the ListView.builder constructor lazily
       // builds Widgets as they’re scrolled into view.
-      body: ListView.builder(
-        // Providing a restorationId allows the ListView to restore the
-        // scroll position when a user leaves and returns to the app after it
-        // has been killed while running in the background.
-        restorationId: 'sampleItemListView',
-        itemCount: items.length,
-        itemBuilder: (BuildContext context, int index) {
-          final item = items[index];
+      body: FutureBuilder<List<User>>(
+          future: futureUsers,
+          builder: (context, snapshot) {
+            var data = snapshot.data;
+            if (data == null) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            return ListView.builder(
+              // Providing a restorationId allows the ListView to restore the
+              // scroll position when a user leaves and returns to the app after it
+              // has been killed while running in the background.
+              restorationId: 'sampleItemListView',
+              itemCount: data.length,
+              itemBuilder: (BuildContext context, int index) {
+                final user = data[index];
 
-          return ListTile(
-              title: Text('user: ${item.name}'),
-              leading: const CircleAvatar(
-                // Display the Flutter Logo image asset.
-                foregroundImage: AssetImage('assets/images/flutter_logo.png'),
-              ),
-              onTap: () {
-                // Navigate to the details page. If the user leaves and returns to
-                // the app after it has been killed while running in the
-                // background, the navigation stack is restored.
-                Navigator.pushNamed(context, UserDetailView.routeName,
-                    arguments: item);
-              });
-        },
-      ),
+                return ListTile(
+                    title: Text('user: ${user.name}'),
+                    leading: const CircleAvatar(
+                      // Display the Flutter Logo image asset.
+                      foregroundImage:
+                          AssetImage('assets/images/flutter_logo.png'),
+                    ),
+                    onTap: () {
+                      // Navigate to the details page. If the user leaves and returns to
+                      // the app after it has been killed while running in the
+                      // background, the navigation stack is restored.
+                      Navigator.pushNamed(context, UserDetailView.routeName,
+                          arguments: user);
+                    });
+              },
+            );
+          }),
     );
+  }
+
+  Future<List<User>> fetchUsers() async {
+    final response = await http
+        .get(Uri.parse('https://api-go-triplan.up.railway.app/users'));
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return List.of(jsonDecode(utf8.decode(response.bodyBytes)))
+          .map((u) => User.fromJson(u))
+          .toList();
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load User');
+    }
   }
 }
