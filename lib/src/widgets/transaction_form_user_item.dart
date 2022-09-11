2374c89aba8d6b1dd0575f48f0ca054cc0048e5e
Léon Delcroix <leondecheux@gmail.com>
2022-09-11 14:45:19 +0200

    transaction creation form

---
+++ lib/src/widgets/transaction_form_user_item.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:triplan/src/models/transaction.dart';
+
+import '../models/user.dart';
+
+class TransactionFormUserItem extends StatefulWidget {
+  const TransactionFormUserItem(
+      {required this.user, required this.onChanged, super.key});
+
+  final User user;
+  final void Function(Map<User, TransactionTarget?>?) onChanged;
+
+  @override
+  State<TransactionFormUserItem> createState() =>
+      _TransactionFormUserItemState();
+}
+
+class _TransactionFormUserItemState extends State<TransactionFormUserItem> {
+  final _weight = TextEditingController();
+
+  void updateTrigger() {
+    TransactionTarget target = TransactionTarget(
+      userId: widget.user.id,
+      weight: int.parse(_weight.text),
+    );
+    var map = <User, TransactionTarget>{};
+    map[widget.user] = target;
+    widget.onChanged(map);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text('user: ${widget.user.name}'),
+      trailing: SizedBox(
+          width: 50,
+          child: TextFormField(
+            onChanged: (value) => {updateTrigger()},
+            controller: _weight,
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+              FilteringTextInputFormatter.digitsOnly
+            ],
+          )),
+    );
+  }
+}
